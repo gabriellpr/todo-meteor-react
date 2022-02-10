df725d4b2478a1cfb4f4026ce82d90debc18071e
Gabriel Porfirio <gabrielporfirio1994@gmail.com>
2022-02-10 16:53:11 -0300

    first commit

---
+++ simple-todos-react/imports/ui/TaskForm.jsx
@@ -0,0 +1,32 @@
+import { Meteor } from 'meteor/meteor';
+import React, { useState } from 'react';
+
+
+export const TaskForm = () => {
+    const [text, setText] = useState("");
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        
+        if(!text) return;
+
+        Meteor.call('tasks.insert', text);
+
+        setText("");
+    }
+
+    return (
+        <form className="task-form" onSubmit={handleSubmit}>
+            <input 
+                type="text"
+                placeholder="Adicione uma nova tarefa"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
+
+            <button type="submit">
+                Adicionar tarefa
+            </button>
+        </form>
+    );
+};
